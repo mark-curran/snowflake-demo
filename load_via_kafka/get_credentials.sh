f2a54044b4bf714eea626c1614f9dddebbca8452
Mark Curran <markcurran@Marks-MacBook-Air.local>
2025-04-01 15:24:37 +1100

    Extract bootstrap server in get credentials script.

--- load_via_kafka/get_credentials.sh
+++ load_via_kafka/get_credentials.sh
@@ -10,11 +10,12 @@ terraform_output=$(terraform output -json)
 
 # Extract the "primary_connection_string" value
 connection_string=$(echo "$terraform_output" | jq -r '.primary_connection_string.value')
+bootstrap_server=$(echo $connection_string | awk -F\/ '{print $3":9093"}')
 
 # Return to the original directory
 popd > /dev/null
 
 # Write the extracted value to the JSON file
-echo "{ \"primary_connection_string\": \"$connection_string\" }" > ../event_hub_connection.json
+echo "{ \"primary_connection_string\": \"$connection_string\", \"bootstrap_server\": \"$bootstrap_server\" }" > ../event_hub_connection.json
 
 echo "Primary connection string has been saved to event_hub_connection.json"
