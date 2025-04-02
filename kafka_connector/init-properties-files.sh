bc98578053bae38469e4db047f514d7e05294377
Mark Curran <markcurran@Marks-MacBook-Air.local>
2025-04-02 11:44:33 +1100

    Established connection to Snowflake.

--- kafka_connector/init-properties-files.sh
+++ kafka_connector/init-properties-files.sh
@@ -3,13 +3,23 @@
 SECRET_PATH="/run/secrets/event_hub_credentials"
 PROPERTIES_FILES_TEMPLATE="/tmp/shared-properties/template-connect-standalone.properties"
 PROPERTIES_FILE="/shared-properties/connect-standalone.properties"
+ADMIN_PROPERTIES_TEMPLATE="/tmp/shared-properties/template-admin-client.properties"
+ADMIN_PROPERTIES_FILE="/shared-properties/admin-client.properties"
 
 PRIMARY_CONNECTION_STRING=$(jq -r '.primary_connection_string' "$SECRET_PATH")
 BOOTSTRAP_SERVER=$(jq -r '.bootstrap_server' "$SECRET_PATH")
+USERNAME='$ConnectionString'
 
 env PRIMARY_CONNECTION_STRING="$PRIMARY_CONNECTION_STRING" \
 env BOOTSTRAP_SERVER="$BOOTSTRAP_SERVER" \
+env USERNAME="$USERNAME" \
 envsubst < tmp/shared-properties/template-connect-standalone.properties \
 > "$PROPERTIES_FILE"
+
+env PRIMARY_CONNECTION_STRING="$PRIMARY_CONNECTION_STRING" \
+env BOOTSTRAP_SERVER="$BOOTSTRAP_SERVER" \
+env USERNAME="$USERNAME" \
+envsubst < $ADMIN_PROPERTIES_TEMPLATE \
+> "$ADMIN_PROPERTIES_FILE"
 
 echo 'Secrets injected into properties file.'
