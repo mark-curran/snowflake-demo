549c39543252fd84b67784ae2c6871459e2ddac6
Mark Curran <markcurran@Marks-MacBook-Air.local>
2025-05-13 11:25:59 +1000

    Fix envvar templating and terraform outputs.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,10 @@ output "primary_connection_string" {
   value = module.event_hub.primary_connection_string
   sensitive = true
 }
+
+# Example primary connection string: "Endpoint=sb://big-apple.servicebus.windows.net/;SharedAccessKeyName=test-auth-rule;SharedAccessKey=helloworld"
+# Example bootstrap server: "big-apple.servicebus.windows.net:9093"
+output "bootstrap_server" {
+  value =  "${split("/;",split("sb://",module.event_hub.primary_connection_string)[1])[0]}:9093"
+  sensitive = true
+}
